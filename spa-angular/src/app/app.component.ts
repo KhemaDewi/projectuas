ffe931f5d7dde6d9153a301002716fb403c588b9
nikooo08 <nikocrysostomus_2226240010@mhs.mdp.ac.id>
2024-12-27 21:09:43 +0700

    auth

--- spa-angular/src/app/app.component.ts
+++ spa-angular/src/app/app.component.ts
@@ -6,13 +6,25 @@ import { Router } from '@angular/router';  // Mengimpor Router
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterOutlet, RouterModule, CommonModule], // Menambahkan RouterOutlet, RouterModule
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'spa-angular';
+
+  isLoggedIn: boolean = false; // Menyimpan status login
   constructor(private router: Router) { } // Menambahkan router pada konstruktor
-  
+
+  ngOnInit() {
+    // Memeriksa apakah ada token di localStorage
+    this.isLoggedIn = !!localStorage.getItem('authToken');
+  }
+
+  onLogout() {
+    // Menghapus token dari localStorage saat logout
+    localStorage.removeItem('authToken');
+    this.isLoggedIn = false; // Mengubah status login menjadi false
+    this.router.navigate(['/auth']); // Arahkan ke halaman login setelah logout
+  }
 }
